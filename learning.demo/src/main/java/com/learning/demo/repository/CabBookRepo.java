5357f892efcf4b8a0761f6581df7481c3511c6b6
abhishek-ksolves <abhishek.rathour@ksolves.com>
2022-12-09 15:29:03 +0530

    fifth

--- learning.demo/src/main/java/com/learning/demo/repository/CabBookRepo.java
+++ learning.demo/src/main/java/com/learning/demo/repository/CabBookRepo.java
@@ -4,5 +4,4 @@ import com.learning.demo.entities.CabBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CabBookRepo extends JpaRepository<CabBook, Integer> {
-    findBy
 }
